502b411f6ba1478c7fd24c485ae64146869cf5e0
Ilya Povarov <56920543+holybobrius@users.noreply.github.com>
2021-05-17 23:15:01 +0300

    form post to api

--- src/components/NewQuoteModal.tsx
+++ src/components/NewQuoteModal.tsx
@@ -1,6 +1,7 @@
 import { FC, useState, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from 'axios'
+import { useForm } from 'react-hook-form';
 
 const Modal = styled.div`
     display: block; /* Hidden by default */
@@ -13,86 +14,92 @@ const Modal = styled.div`
     overflow: auto; /* Enable scroll if needed */
     background-color: rgb(0,0,0); /* Fallback color */
     background-color: rgba(0,0,0,0.7);
-    `;
+`;
 
-    const ModalContent = styled.div`
-        background-color: #191B1F;
-        margin: 15% auto; /* 15% from the top and centered */
-        padding: 20px;
-        border-radius: 15px;
-        width: 30%; 
-        color: #aaa;
-    `;
+const ModalContent = styled.div`
+    background-color: #191B1F;
+    margin: 15% auto; /* 15% from the top and centered */
+    padding: 20px;
+    border-radius: 15px;
+    width: 30%; 
+    color: #aaa;
+`;
 
-    const Form = styled.form`
-        display: flex;
-        flex-direction: column;
-        gap: 15px;
-        align-items: center;
-        justify-items: center;
-    `;
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    align-items: center;
+    justify-items: center;
+`;
 
-    const TextArea = styled.textarea`
-        background-color: #2e3136;
-        border-style: none;
-        border-radius: 10px;
-        height: 15vh;
-        font-size: 1rem;
-        width: 80%;
-    `;
+const TextArea = styled.textarea`
+    background-color: #2e3136;
+    border-style: none;
+    border-radius: 10px;
+    height: 15vh;
+    font-size: 1rem;
+    width: 80%;
+`;
 
-    const Select = styled.select`
-        background-color: #2e3136;
-        border-style: none;
-        border-radius: 10px;
-        font-size: 1rem;
-        width: 20%;
-    `;
+const Select = styled.select`
+    background-color: #2e3136;
+    border-style: none;
+    border-radius: 10px;
+    font-size: 1rem;
+    width: 20%;
+`;
 
-    const Date = styled.input`
-        background-color: #2e3136;
-        border-style: none;
-        border-radius: 10px;
-        font-size: 1rem;
-        width: 25%;
-        padding: 3px;
-    `;
+const Date = styled.input`
+    background-color: #2e3136;
+    border-style: none;
+    border-radius: 10px;
+    font-size: 1rem;
+    width: 25%;
+    padding: 3px;
+`;
 
-    const Submit = styled.input`
-        background-color: #2e3136;
-        border-style: none;
-        border-radius: 15px;
-        margin-top: 10px;
-        height: 4vh;
-        width: 8vw;
-        cursor: pointer;
-        transition: 200ms ease;
-        &:hover {
-            transform: scale(1.03);
-        }
-    `;
-
-    const Close = styled.span`
-        color: #aaa;
-        float: right;
-        font-size: 28px;
-        font-weight: bold;
-        &:hover, :focus {
-        color: black;
-        text-decoration: none;
-        cursor: pointer;
-        }
-    `;
-
-    const Label = styled.label`
-        font-size: 1rem;
-        font-weight: bold;
-    `;
-
-    interface Props {
-        visible: boolean;
-        changeVisibility: () => void;
+const Submit = styled.input`
+    background-color: #2e3136;
+    border-style: none;
+    border-radius: 15px;
+    margin-top: 10px;
+    height: 4vh;
+    width: 8vw;
+    cursor: pointer;
+    transition: 200ms ease;
+    &:hover {
+        transform: scale(1.03);
     }
+`;
+
+const Close = styled.span`
+    color: #aaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+    &:hover, :focus {
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+    }
+`;
+
+const Label = styled.label`
+    font-size: 1rem;
+    font-weight: bold;
+`;
+
+interface Props {
+    visible: boolean;
+    changeVisibility: () => void;
+}
+
+type FormValues = {
+    quote: string,
+    author: string,
+    date: string
+}
 
 const NewQuoteModal: FC<Props> = (props) => {
     const [users, setUsers] = useState<any[]>([]);
@@ -102,22 +109,32 @@ const NewQuoteModal: FC<Props> = (props) => {
                 setUsers(r.data);
             })();
     }, []);
+
+    const { register, handleSubmit } = useForm<FormValues>();
     return(
         props.visible ?
             <Modal id="modal">
                 <ModalContent>
                     <Close onClick={props.changeVisibility}>&times;</Close>
-                    <Form>
+                    <Form onSubmit={handleSubmit(data => {
+                            axios.post('url', {
+                                quote: data.quote,
+                                author: data.author,
+                                date: data.date
+                            })
+                            console.log(data);
+                        })}
+                    >
                         <Label htmlFor="quote">Цитата</Label>
-                        <TextArea name="quote"></TextArea>
+                        <TextArea {...register("quote", { required: true })}></TextArea>
                         <Label htmlFor="author">Автор</Label>
-                        <Select name="author">
+                        <Select {...register("author", { required: true })}>
                             {users.map(user => (
                                 <option>{user.name}</option>
                             ))}
                         </Select>
                         <Label htmlFor="date">Дата</Label>
-                        <Date name="date" type="date"></Date>
+                        <Date {...register("date", { required: true })} type="date"></Date>
                         <Submit type="submit"></Submit>
                     </Form>
                 </ModalContent>
