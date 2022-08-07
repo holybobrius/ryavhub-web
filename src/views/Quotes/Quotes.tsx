513dfd9917f71382e85b7a3dd0a244f02f3fe185
Ilya Povarov <56920543+holybobrius@users.noreply.github.com>
2022-08-07 16:10:45 +0300

    Quotes markup refactor

--- src/views/Quotes/Quotes.tsx
+++ src/views/Quotes/Quotes.tsx
@@ -1,62 +1,96 @@
-import { FC, useState, useEffect } from 'react'
-import Quote from '../../components/Quote/Quote'
-import * as Styled from './Quotes.style'
-import axios from 'axios'
-import NewQuoteModal from '../../components/NewQuoteModal/NewQuoteModal';
-import AddButton from '../../components/AddButton/AddButton'
-import { useSelector } from 'react-redux';
-import { RootState } from '../../store';
-import LockedHome from '../LockedHome/LockedHome';
-import backgroundImg from '../../assets/images/background.png'
+import { FC, useState, useEffect } from "react";
+import Quote from "../../components/Quote/Quote";
+import axios from "axios";
+import NewQuoteModal from "../../components/NewQuoteModal/NewQuoteModal";
+import AddButton from "../../components/AddButton/AddButton";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+import LockedHome from "../LockedHome/LockedHome";
+import backgroundImg from "../../assets/images/background.png";
+import "./Quotes.css";
+import QuotesNavPanel from "../../components/QuotesNavPanel/QuotesNavPanel";
 
 interface QuotesUser {
-    id: number;
-    email: string;
-    name: string;
- }
+  id: number;
+  email: string;
+  name: string;
+}
 
- interface QuotesApiResponseEntity {
-    id: number;
-    quote: string;
-    quote_by: QuotesUser;
-    created_by: QuotesUser;
-    date: string;
- }
+interface QuotesApiResponseEntity {
+  id: number;
+  quote: string;
+  quote_by: QuotesUser;
+  created_by: QuotesUser;
+  date: string;
+}
 
 const Quotes: FC = () => {
-    const [quotes, setQuotes] = useState<QuotesApiResponseEntity[]>([]);
-    const [isVisible, setIsVisible] = useState<boolean>(false);
-    const reduxStore = useSelector<RootState>(state => state.googleUser)
-    const changeVisibility = () => {
-        setIsVisible(!isVisible);
-    }
-    const fetchQuotes = async () => {
-        const r = await axios.get<QuotesApiResponseEntity[]>('https://api.ryav.tk/v1/quotes');
-        setQuotes(r.data.reverse());
-    }
-    useEffect(() => { fetchQuotes() }, []);
+  const [quotes, setQuotes] = useState<QuotesApiResponseEntity[]>([]);
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const reduxStore = useSelector<RootState>((state) => state.googleUser);
+  const changeVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+  const fetchQuotes = async () => {
+    const r = await axios.get<QuotesApiResponseEntity[]>(
+      "https://api.ryav.tk/v1/quotes"
+    );
+    setQuotes(r.data.reverse());
+  };
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
 
-    if(reduxStore === null) {
-        return(
-            <LockedHome />
-        )
-    }
+  if (reduxStore === null) {
+    return <LockedHome />;
+  }
 
-    return (
-        <Styled.QuotesPage style={{backgroundImage: `url(${backgroundImg})`}}>
-            <Styled.QuotesContainer>
-                {quotes.map(quote => (
-                    <Quote
-                        key={quote.id}
-                        quote={quote.quote}
-                        date={quote.date}
-                        author={quote.quote_by.name}
-                    />
-                ))}
-            </Styled.QuotesContainer>
-            <AddButton handleClick={changeVisibility}/>
-            <NewQuoteModal visible={isVisible} changeVisibility={changeVisibility} fetchQuotes={fetchQuotes}/>
-        </Styled.QuotesPage>
-    )
-}
+  return (
+    <section className="quotes-page" id="quotesContainer">
+      <QuotesNavPanel changeVisibility={changeVisibility} />
+      <div className="quotes-container">
+        {quotes.map((quote) => (
+          <Quote
+            key={quote.id}
+            quote={quote.quote}
+            date={quote.date}
+            author={quote.quote_by.name}
+          />
+        ))}
+      </div>
+      <div className="img-box">
+        <img
+          src={require("../../assets/images/Композиция 1_тест мне интересно.png")}
+        ></img>
+      </div>
+      {/*<AddButton handleClick={changeVisibility}/>*/}
+      <NewQuoteModal
+        visible={isVisible}
+        changeVisibility={changeVisibility}
+        fetchQuotes={fetchQuotes}
+      />
+    </section>
+  );
+
+  //   <Styled.QuotesPage id="quotesContainer">
+  //       <QuotesNavPanel changeVisibility={changeVisibility} />
+  //       <Styled.QuotesContainer>
+  //         {quotes.map((quote) => (
+  //           <Quote
+  //             key={quote.id}
+  //             quote={quote.quote}
+  //             date={quote.date}
+  //             author={quote.quote_by.name}
+  //           />
+  //         ))}
+  //       </Styled.QuotesContainer>
+
+  //       {/*<AddButton handleClick={changeVisibility}/>*/}
+  //       <NewQuoteModal
+  //         visible={isVisible}
+  //         changeVisibility={changeVisibility}
+  //         fetchQuotes={fetchQuotes}
+  //       />
+  //     </Styled.QuotesPage>
+};
 export default Quotes;
