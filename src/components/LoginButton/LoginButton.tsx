7ad9b9848ad5224935f6a4db09d799fdc359a69f
Ilya Povarov <56920543+holybobrius@users.noreply.github.com>
2022-08-11 14:03:57 +0300

    LoginBtn refactor

--- src/components/LoginButton/LoginButton.tsx
+++ src/components/LoginButton/LoginButton.tsx
@@ -1,58 +1,67 @@
 import { FC } from "react";
-import { useSelector, useDispatch } from 'react-redux'
-import { GoogleLogin, GoogleLogout, GoogleLoginResponse, GoogleLoginResponseOffline } from "react-google-login";
-import { RootState } from '../../store'
-import { googleLogin, googleLogout } from '../../store/actions/auth'
-import * as Styled from './LoginButton.style'
+import { useSelector, useDispatch } from "react-redux";
+import {
+  GoogleLogin,
+  GoogleLogout,
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline,
+} from "react-google-login";
+import { RootState } from "../../store";
+import { googleLogin, googleLogout } from "../../store/actions/auth";
+
+import "./LoginButton.css";
 
 const LoginButton: FC = () => {
-    const user = useSelector((s: RootState) => s.googleUser)
-    const dispatch = useDispatch();
+  const user = useSelector((s: RootState) => s.googleUser);
+  const dispatch = useDispatch();
 
-    //todo send request to /account/auth to check if user is in database (onSuccess)
-    const onSuccess = (data: GoogleLoginResponse | GoogleLoginResponseOffline) => {
-        if(!('tokenId' in data)) {
-            alert('offline)')
-            return;
-        }
-        googleLogin(data as GoogleLoginResponse)
-            .then(r => dispatch(r))
-            .catch(e => {
-                alert('error (see console)')
-                console.log(e)
-            })
-         //make request to this thang to check shit
+  //todo send request to /account/auth to check if user is in database (onSuccess)
+  const onSuccess = (
+    data: GoogleLoginResponse | GoogleLoginResponseOffline
+  ) => {
+    if (!("tokenId" in data)) {
+      alert("offline)");
+      return;
     }
-    const onFailure = (err: any) => {
-        alert('unsuccesful')
-        console.log(err)
-    }
-    const onLogout = () => {
-        dispatch(googleLogout())
-    }
+    googleLogin(data as GoogleLoginResponse)
+      .then((r) => dispatch(r))
+      .catch((e) => {
+        alert("error (see console)");
+        console.log(e);
+      });
+    //make request to this thang to check shit
+  };
+  const onFailure = (err: any) => {
+    alert("unsuccesful");
+    console.log(err);
+  };
+  const onLogout = () => {
+    dispatch(googleLogout());
+  };
 
-    return user ?
-    (
-        <GoogleLogout
-            clientId="68682133883-q7k867bb1i2vjgg778kfr5c6vdso1edh.apps.googleusercontent.com"
-            onLogoutSuccess={onLogout}
-            render={props => (
-                <Styled.NavLoginBtn onClick={props.onClick}>Logout</Styled.NavLoginBtn>
-            )}
-        />
-    )
-    :
-    (
-        <GoogleLogin
-            clientId="68682133883-q7k867bb1i2vjgg778kfr5c6vdso1edh.apps.googleusercontent.com"
-            onSuccess={onSuccess}
-            onFailure={onFailure}
-            cookiePolicy='single_host_origin'
-            isSignedIn={true}
-            render={props => (
-                <Styled.LoginBtn onClick={props.onClick}>Login</Styled.LoginBtn>
-            )}
-        />
-    )
-}
+  return user ? (
+    <GoogleLogout
+      clientId="68682133883-q7k867bb1i2vjgg778kfr5c6vdso1edh.apps.googleusercontent.com"
+      onLogoutSuccess={onLogout}
+      render={(props) => (
+        <button className="login-btn--nav" onClick={props.onClick}>
+          Logout
+        </button>
+      )}
+    />
+  ) : (
+    <GoogleLogin
+      clientId="68682133883-q7k867bb1i2vjgg778kfr5c6vdso1edh.apps.googleusercontent.com"
+      onSuccess={onSuccess}
+      onFailure={onFailure}
+      cookiePolicy="single_host_origin"
+      isSignedIn={true}
+      render={(props) => (
+        <button className="login-btn" onClick={props.onClick}>
+          Login
+        </button>
+      )}
+    />
+  );
+};
 export default LoginButton;
