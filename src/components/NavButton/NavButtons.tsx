3ec31daf734a72ce9de46248af8ff4b6c2d2abec
Ilya Povarov <56920543+holybobrius@users.noreply.github.com>
2022-08-11 14:12:26 +0300

    NavButtons refactor

--- src/components/NavButton/NavButtons.tsx
+++ src/components/NavButton/NavButtons.tsx
@@ -1,32 +1,35 @@
-import * as Styled from './NavButtons.style'
-import { FC, useMemo } from 'react'
+import "./NavButtons.css";
+import { FC, useMemo } from "react";
 
 interface Props {
-    total: number
+  total: number;
 }
 
 const NavButtons: FC<Props> = (props) => {
-    const scroll: any = (n: number) => {
-        const element = document.querySelector(`#card${n}`);
-        element?.scrollIntoView({behavior: 'smooth'});
+  const scroll: any = (n: number) => {
+    const element = document.querySelector(`#card${n}`);
+    element?.scrollIntoView({ behavior: "smooth" });
+  };
+  const buttons = useMemo(() => {
+    let btns = [];
+    for (let i = 0; i < props.total; i++) {
+      btns.push(
+        <label key={`radio${i}`}>
+          <input
+            className="nav-btns-input"
+            type="radio"
+            name="radio-btn"
+            onChange={scroll(i)}
+            id={`radio${i}`}
+            onClick={() => scroll(i)}
+          />
+          <span className="nav-btns-span" />
+        </label>
+      );
     }
-    const buttons = useMemo(() => {
-        let btns = [];
-        for(let i = 0; i < props.total; i++) {
-            btns.push(
-                <label key={`radio${i}`}>
-                    <Styled.NavButtonInput type="radio" name="radio-btn" onChange={scroll(i)} id={`radio${i}`} onClick={() => scroll(i)}/>
-                    <Styled.NavButtonSpan />
-                </label>
-                );
-        }
-        return btns;
-    }, [props.total])    
-    return (
-        <Styled.Container>
-            {buttons}
-        </Styled.Container>
-    )
-}
+    return btns;
+  }, [props.total]);
+  return <div className="nav-btns-container">{buttons}</div>;
+};
 
 export default NavButtons;
