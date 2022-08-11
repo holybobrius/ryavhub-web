240c3d3a7c7e5e92660d0049c827d0340db92a85
Ilya Povarov <56920543+holybobrius@users.noreply.github.com>
2022-08-11 13:26:26 +0300

    GameSaves markup refactor

--- src/views/GameSaves/GameSaves.style.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const CardsContainer = styled.div`
-    background-color: #191B1F;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-`
--- src/views/GameSaves/GameSaves.tsx
+++ src/views/GameSaves/GameSaves.tsx
@@ -1,55 +1,54 @@
-import { FC, useState, useEffect } from 'react'
-import SaveCard from "../../components/SaveCard/SaveCard"
-import * as Styled from './GameSaves.style'
-import axios from 'axios'
-import NavButtons from '../../components/NavButton/NavButtons'
-import { useSelector } from 'react-redux'
-import { RootState } from '../../store'
-import LockedHome from '../LockedHome/LockedHome'
+import { FC, useState, useEffect } from "react";
+import SaveCard from "../../components/SaveCard/SaveCard";
+import axios from "axios";
+import NavButtons from "../../components/NavButton/NavButtons";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+import LockedHome from "../LockedHome/LockedHome";
+import "./GameSaves.css";
 
 interface GameSaveApiEntity {
-    id: number,
-    name: string,
-    year: number,
-    download_link: string,
-    size: string,
-    imgs: string[]
+  id: number;
+  name: string;
+  year: number;
+  download_link: string;
+  size: string;
+  imgs: string[];
 }
 
 const GameSaves: FC = () => {
-    const [saves, setSaves] = useState<GameSaveApiEntity[]>([]);
-    const reduxStore = useSelector<RootState>(state => state.googleUser)
-    useEffect(() => {
-        (async () => {
-            const fr = await axios.get<GameSaveApiEntity[]>('https://api.ryav.tk/v1/gamesaves');
-            setSaves(fr.data.reverse());
-        })();
-    }, [])
+  const [saves, setSaves] = useState<GameSaveApiEntity[]>([]);
+  const reduxStore = useSelector<RootState>((state) => state.googleUser);
+  useEffect(() => {
+    (async () => {
+      const fr = await axios.get<GameSaveApiEntity[]>(
+        "https://api.ryav.tk/v1/gamesaves"
+      );
+      setSaves(fr.data.reverse());
+    })();
+  }, []);
 
-    if(reduxStore === null) {
-        return(
-            <LockedHome />
-        )
-    }
+  if (reduxStore === null) {
+    return <LockedHome />;
+  }
 
-    return (
-            <Styled.CardsContainer>
-                {saves.map(save => (
-                    <SaveCard
-                        key={save.id}
-                        title={save.name}
-                        description={save.year.toString()}
-                        imageAlt={save.name}
-                        images={save.imgs}
-                        size={save.size}
-                        link={save.download_link}
-                        total={saves.length}
-                        index={saves.indexOf(save)}
-                    />
-                ))}
-                <NavButtons total={saves.length} />
-            </Styled.CardsContainer>
-    );
-}
-export default GameSaves
-
+  return (
+    <section className="section-saves">
+      {saves.map((save) => (
+        <SaveCard
+          key={save.id}
+          title={save.name}
+          description={save.year.toString()}
+          imageAlt={save.name}
+          images={save.imgs}
+          size={save.size}
+          link={save.download_link}
+          total={saves.length}
+          index={saves.indexOf(save)}
+        />
+      ))}
+      <NavButtons total={saves.length} />
+    </section>
+  );
+};
+export default GameSaves;
