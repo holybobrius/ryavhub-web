f9b64f50da369de2a40a6c1fa050493b848b5d20
Ilya Povarov <ilya.povaroff@gmail.com>
2023-12-02 22:24:59 +0300

    reverse timeline

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -11,7 +11,7 @@ const Timeline: FC<Props> = ({ items }) => {
   if (!items) return null;
   return (
     <div className="timeline-container">
-      {items.map((i: TimelineType.Timeline) => (
+      {items.reverse().map((i: TimelineType.Timeline) => (
         <TimelineItem
           date={i.date}
           place={i.location}
