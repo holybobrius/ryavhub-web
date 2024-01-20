ed6b45b0dc904c6a4acc56e3da3dbcf903563bed
Ilya Povarov <ilya.povaroff@gmail.com>
2024-01-20 18:55:43 +0300

    change login button

--- src/components/LoginButton/LoginButton.tsx
+++ src/components/LoginButton/LoginButton.tsx
@@ -1,48 +1,52 @@
 import { FC, useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { googleLogout, GoogleLogin, CredentialResponse } from '@react-oauth/google'
+import {
+  googleLogout,
+  GoogleLogin,
+  CredentialResponse,
+} from "@react-oauth/google";
 import { RootState } from "../../store";
 import { useHistory } from "react-router-dom";
 import { GOOGLE_LOGIN, GOOGLE_LOGOUT } from "../../store/types";
 import { accountRequest } from "../../requests/account";
 import "./LoginButton.css";
+import { base } from "../../requests/base";
 
 const LoginButton: FC = () => {
-   const user = useSelector((s: RootState) => s.googleUser);
-   const dispatch = useDispatch();
-   const history = useHistory();
+  const user = useSelector((s: RootState) => s.googleUser);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-   const onSuccess = useCallback((data: CredentialResponse) => {
-      if (!data.credential) return alert("offline)");
+  const onSuccess = useCallback((data: CredentialResponse) => {
+    if (!data.credential) return alert("offline)");
 
-      return accountRequest(data.credential)
-         .then(() => dispatch({ type: GOOGLE_LOGIN, payload: data }))
-         .then(() => history.push("/"))
-         .catch((e) => {
-            alert("Smth gone wrong (see console)");
-            console.log(e);
-         });
-   }, []);
+    return accountRequest(data.credential)
+      .then(() => dispatch({ type: GOOGLE_LOGIN, payload: data }))
+      .then(() => history.push("/"))
+      .catch((e) => {
+        alert("Smth gone wrong (see console)");
+        console.log(e);
+      });
+  }, []);
 
-   const onError = useCallback(() => {
-      alert("Smth gone wrong (see console)");
-   }, []);
+  const onError = useCallback(() => {
+    alert("Smth gone wrong (see console)");
+  }, []);
 
-   const onLogout = useCallback(() => {
-      dispatch({ type: GOOGLE_LOGOUT })
-      googleLogout();
-   }, []);
+  const onLogout = useCallback(() => {
+    dispatch({ type: GOOGLE_LOGOUT });
+  }, []);
 
-   return user ? (
+  return user ? (
+    <button className="login-btn--nav" onClick={onLogout}>
+      Выйти
+    </button>
+  ) : (
+    <a href="https://t.me/ryavbot">
       <button className="login-btn--nav" onClick={onLogout}>
-          Logout
+        Войти
       </button>
-   ) : (
-      <GoogleLogin 
-         onSuccess={onSuccess}
-         onError={onError}
-         theme="filled_black"
-      />
-   )
+    </a>
+  );
 };
 export default LoginButton;
