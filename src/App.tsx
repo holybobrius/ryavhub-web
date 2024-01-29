b2bf8d784e13bd2d2d268de7c57f79c9281a2e80
Ilya Povarov <ilya.povaroff@gmail.com>
2024-01-29 18:31:56 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,10 @@ import { CringePg } from "./views/CringePG/CringePg";
 import { CringePgAdmin } from "./views/CringePGAdmin/CringePgAdmin";
 import { useUser } from "./requests/user/useUser";
 import { ConfigProvider } from "antd";
+import { useGetUserQuery } from "./requests/user/userApi";
 
 const App: FC = () => {
-  const user = useUser();
+  const { data: user, isLoading } = useGetUserQuery();
 
   return (
     <GoogleOAuthProvider clientId="68682133883-jnc71s26jtmqdq98klupgeienssmpbr2.apps.googleusercontent.com">
@@ -38,40 +39,42 @@ const App: FC = () => {
           <div className="app">
             <main>
               <Navbar />
-              <Switch>
-                <GuardedRoute
-                  exact
-                  path="/"
-                  auth={!!user}
-                  render={(props) => <Home {...props} />}
-                />
-                <GuardedRoute
-                  path="/quotes"
-                  auth={!!user}
-                  render={(props) => <QuotesPage {...props} />}
-                />
-                <GuardedRoute
-                  path="/gamesaves"
-                  auth={!!user}
-                  render={(props) => <GameSaves {...props} />}
-                />
-                <GuardedRoute
-                  path="/timeline"
-                  auth={!!user}
-                  render={(props) => <TimelinePage {...props} />}
-                />
-                <GuardedRoute
-                  path="/cringepg"
-                  auth={!!user}
-                  render={(props) => <CringePg {...props} />}
-                />
-                <GuardedRoute
-                  path="/cringepg-admin"
-                  auth={!!user}
-                  render={(props) => <CringePgAdmin {...props} />}
-                />
-                <Route path="/unauth" component={LockedHome} />
-              </Switch>
+              {!isLoading && (
+                <Switch>
+                  <GuardedRoute
+                    exact
+                    path="/"
+                    auth={!!user}
+                    render={(props) => <Home {...props} />}
+                  />
+                  <GuardedRoute
+                    path="/quotes"
+                    auth={!!user}
+                    render={(props) => <QuotesPage {...props} />}
+                  />
+                  <GuardedRoute
+                    path="/gamesaves"
+                    auth={!!user}
+                    render={(props) => <GameSaves {...props} />}
+                  />
+                  <GuardedRoute
+                    path="/timeline"
+                    auth={!!user}
+                    render={(props) => <TimelinePage {...props} />}
+                  />
+                  <GuardedRoute
+                    path="/cringepg"
+                    auth={!!user}
+                    render={(props) => <CringePg {...props} />}
+                  />
+                  <GuardedRoute
+                    path="/cringepg-admin"
+                    auth={!!user}
+                    render={(props) => <CringePgAdmin {...props} />}
+                  />
+                  <Route path="/unauth" component={LockedHome} />
+                </Switch>
+              )}
             </main>
           </div>
         </ConfigProvider>
